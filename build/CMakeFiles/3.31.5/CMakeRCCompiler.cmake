dafb2849ca1b11a461e910fa0ab1e9ab99c621ff
Ridwan Hidayatullah <24.ridwanhidayatullah.9b@gmail.com>
2026-07-07 08:28:57 +0700

    remove build folder

--- build/CMakeFiles/3.31.5/CMakeRCCompiler.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_RC_COMPILER "C:/Users/Ridwan Hidayatullah/Documents/codeblocks-25.03mingw-nosetup/MinGW/bin/llvm-rc.exe")
-set(CMAKE_RC_COMPILER_ARG1 "")
-set(CMAKE_RC_COMPILER_LOADED 1)
-set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .res)
-set(CMAKE_RC_COMPILER_ENV_VAR "RC")
